8b3202755a7060348bc23020aa26af40e2b768f8
moises20066 <163145734+moises20066@users.noreply.github.com>
2024-11-14 08:50:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    "geografia",
+    "qual a capital da china?'",
+    "pekim"
+)
+
+criaCartao(
+    "Química",
+    "qual o modelo de dalton?",
+    "modelo de brilhar"
+)
+
+criaCartao(
+    "historia",
+    "em que ano se independizou venenzuela? ",
+    "1811"
+)
+
+criaCartao(
+    "Português",
+    "Quem escreveu Dom Casmurro",
+    "Machado de Assis"
+)
+
+criaCartao(
+    "ingles",
+    "oi em ingles",
+    "oi"
+)
+criaCartao(
+    "Espanhol",
+    "O que significa silla",
+    "cadeira"
+)
